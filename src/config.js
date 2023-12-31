de84ddb6209d38f0e8f4923e9c81e6b7bdfacb53
Omowunmi <sogunledolapo@gmail.com>
2023-12-31 16:32:40 +0100

    initial commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    sections: ["home", "skills", "projects", "contact"],
+    home: {
+        title: 'WUNMIS',
+        subtitle:'DEVELOPER.'
+    },
+    contact:{
+        mail: 'sogunledolapo@gmail.com'
+    }
+}
